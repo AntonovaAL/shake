831a601a13ab968a8a4ec38a5a68f44fcb5365cb
Анастасия Антонова <antonovanastya11@gmail.com>
2021-05-25 15:38:30 +0300

    дописала функциональность игры, редактирование пробелов и ошибок в коде

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
-const canvas = document.getElementbyId("game");
+const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-//изображение игры 
+
 const ground = new Image();
 ground.src = "img/ground.png";
 
@@ -23,7 +23,7 @@ let snake = [];
 		y: 10 * box
 	};
 
-	document.addEventListen("keydown", direction);
+	document.addEventListener("keydown", direction);
 
 	let dir;
 
@@ -48,11 +48,44 @@ function drawGame() {
 	ctx.drawImage(ground, 0, 0);
 	ctx.drawImage(foodImg, food.x, food.y);
 
-	for(let i = 0; i < shake.length; i++) {
+	for(let i = 0; i < snake.length; i++) {
 		ctx.fillStyle = i == 0 ? "green" : "red";
 		ctx.fillRect(snake[i].x, snake[i].y, box, box);
 	}
-	
+	ctx.fillStyle = "while";
+	ctx.font = "50px Arial";
+	ctx.fillText(score, box * 2.5, box * 1.7);
+
+	let snakeX = snake[0].x;
+	let snakeY = snake[0].y;
+
+	if(snakeX == food.x && snakeY == food.y) {
+		score++;
+		food = {
+			x: Math.floor((Math.random() * 17 + 1)) * box,
+			y: Math.floor((Math.random() * 15 + 3)) * box,
+		};
+	} else{
+			snake.pop();
+	}
+
+	if(dir == "left") snakeX -= box;
+	if(dir == "right") snakeX += box;
+	if(dir == "up") snakeY -= box;
+  if(dir == "down") snakeY += box;
+
+  let newHead = {
+  	x: snakeX,
+  	y: snakeY
+  };
+
+  eatTail(newHead, snake);
+
+  snake.unshift(newHead);
 }
+
+let game = setInterval(drawGame, 250);
+
+
 
 
